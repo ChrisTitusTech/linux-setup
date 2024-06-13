c66cf5ad12de2902907c96546977d8c56dfabd77
Chris Titus <contact@christitus.com>
2024-06-13 15:31:51 -0500

    gaming not finished

--- 2-gaming-setup.sh
+++ 2-gaming-setup.sh
@@ -5,28 +5,6 @@ RED='\e[31m'
 YELLOW='\e[33m'
 GREEN='\e[32m'
 
-# Check if the home directory and linuxtoolbox folder exist, create them if they don't
-LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
-
-if [[ ! -d "$LINUXTOOLBOXDIR" ]]; then
-    echo -e "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
-    mkdir -p "$LINUXTOOLBOXDIR"
-    echo -e "${GREEN}linuxtoolbox directory created: $LINUXTOOLBOXDIR${RC}"
-fi
-
-if [[ ! -d "$LINUXTOOLBOXDIR/linux-setup" ]]; then
-    echo -e "${YELLOW}Cloning linux-setup repository into: $LINUXTOOLBOXDIR/linux-setup${RC}"
-    git clone https://github.com/ChrisTitusTech/linux-setup "$LINUXTOOLBOXDIR/linux-setup"
-    if [[ $? -eq 0 ]]; then
-        echo -e "${GREEN}Successfully cloned linux-setup repository${RC}"
-    else
-        echo -e "${RED}Failed to clone linux-setup repository${RC}"
-        exit 1
-    fi
-fi
-
-cd "$LINUXTOOLBOXDIR/linux-setup"
-
 command_exists() {
     command -v $1 >/dev/null 2>&1
 }
@@ -40,7 +18,7 @@ checkEnv() {
     fi
 
     ## Check Package Handeler
-    PACKAGEMANAGER='apt yum dnf pacman zypper'
+    PACKAGEMANAGER='apt-get yum dnf pacman zypper'
     for pgm in ${PACKAGEMANAGER}; do
         if command_exists ${pgm}; then
             PACKAGER=${pgm}
@@ -79,7 +57,6 @@ checkEnv() {
 
 installDepend() {
     ## Check for dependencies.
-    DEPENDENCIES='tar tree multitail tldr trash-cli unzip cmake make jq'
     echo -e "${YELLOW}Installing dependencies...${RC}"
     if [[ $PACKAGER == "pacman" ]]; then
         if ! grep -q "^\s*\[multilib\]" /etc/pacman.conf; then
@@ -105,33 +82,44 @@ installDepend() {
             echo "No AUR helper found. Please install yay or paru."
             exit 1
         fi
-        ${AUR_HELPER} --noconfirm -S ${DEPENDENCIES}
-    elif [[ $PACKAGER == "apt" ]]; then
-        COMPILEDEPS='build-essential'
+        ${AUR_HELPER} --noconfirm -S wine giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
+lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+    elif [[ $PACKAGER == "apt-get" ]]; then
         sudo ${PACKAGER} update
-        sudo dpkg --add-architecture i386
-        sudo ${PACKAGER} update
-        sudo ${PACKAGER} install -y ${DEPENDENCIES} ${COMPILEDEPS} 
-    elif [[ $PACKAGER == "dnf" ]]; then
-        COMPILEDEPS='@development-tools'
-        sudo ${PACKAGER} update
-        sudo ${PACKAGER} config-manager --set-enabled powertools
-        sudo ${PACKAGER} install -y ${DEPENDENCIES} ${COMPILEDEPS}
-        sudo ${PACKAGER} install -y glibc-devel.i686 libgcc.i686
-    elif [[ $PACKAGER == "zypper" ]]; then
-        COMPILEDEPS='patterns-devel-base-devel_basis'
-        sudo ${PACKAGER} refresh
-        sudo ${PACKAGER} install -y ${DEPENDENCIES} ${COMPILEDEPS}
-        sudo ${PACKAGER} install -y libgcc_s1-gcc7-32bit glibc-devel-32bit
+        sudo ${PACKAGER} install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+    elif [[ $PACKAGER == "dnf|zypper" ]]; then
+        sudo ${PACKAGER} install -y wine
     else
         sudo ${PACKAGER} install -y ${DEPENDENCIES}
     fi
 }
 
 install_additional_dependencies() {
-    case $(command -v apt || command -v zypper || command -v dnf || command -v pacman) in
-        *apt)
-            
+    case $(command -v apt-get || command -v zypper || command -v dnf || command -v pacman) in
+        *apt-get)
+            version=$(git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/lutris/lutris |
+                grep -v 'beta' |
+                tail -n1 |
+                cut -d '/' --fields=3)
+
+            version_no_v=$(echo "$version" | tr -d v)
+            wget "https://github.com/lutris/lutris/releases/download/${version}/lutris_${version_no_v}_all.deb"
+
+            # Install the downloaded .deb package using apt-get
+            echo "Installing lutris_${version_no_v}_all.deb"
+            sudo apt-get update
+            sudo apt-get install ./lutris_${version_no_v}_all.deb
+
+            # Clean up the downloaded .deb file
+            rm lutris_${version_no_v}_all.deb
+
+            echo "Lutris Installation complete."
+            echo "Installing steam..."
+            sudo apt-get install -y steam
             ;;
         *zypper)
             
